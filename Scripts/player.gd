1fe9e9a15197a43bec247cfaa7e476263487ba2f
M1kezh1 <zhzhong2@s.sfusd.edu>
2026-03-23 10:50:47 -0700

    Checkpoint 3

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
+signal OnUpdateHealth (health : int)
+signal OnUpdateScore (score : int)
+
 @export var move_speed : float = 100
 @export var acceleration : float = 50
 @export var braking : float = 20
 @export var gravity : float = 500
 @export var jump_force : float = 200
+
+@export var health : int = 3
 
 var move_input : float
 
@@ -29,6 +34,9 @@ func _physics_process(delta):
 func _process(delta):
 	if velocity.x != 0:
 		Sprite.flip_h = velocity.x > 0
+	
+	if global_position.y > 50:
+		game_over()
 
 	_manage_animation()
 
@@ -39,3 +47,23 @@ func _manage_animation():
 		anim.play("move")
 	else:
 		anim.play("idle")
+
+func take_damage(amount : int):
+	health -= amount
+	OnUpdateHealth.emit(health)
+	_damage_flash()
+
+	if health <= 0:
+		call_deferred("game_over")
+
+func game_over():
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
+func increase_score(amount : int):
+	PlayerStats.score += amount
+	OnUpdateScore.emit(PlayerStats.score)
+
+func _damage_flash():
+	Sprite.modulate = Color.RED
+	await get_tree().create_timer(0.05).timeout
+	Sprite.modulate = Color.WHITE
