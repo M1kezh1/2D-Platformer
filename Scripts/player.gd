7378d60fdb8808c66cb2bb05edd0ee54f2a77c25
M1kezh1 <zhzhong2@s.sfusd.edu>
2026-04-09 12:03:14 -0700

    Climbable Ladder

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,30 +8,50 @@ signal OnUpdateScore (score : int)
 @export var braking : float = 20
 @export var gravity : float = 500
 @export var jump_force : float = 200
-
 @export var health : int = 3
 
 var move_input : float
+var is_on_rope = false
 
 @onready var Sprite : Sprite2D = $Sprite
 @onready var anim : AnimationPlayer = $AnimationPlayer
 @onready var audio : AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var tile_map: TileMapLayer = $"../TileMapLayer"
 
 var take_damage_sfx : AudioStream = preload("res://Audio/take_damage.wav")
 var coin_sfx : AudioStream = preload("res://Audio/coin.wav")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	var detection_pos = global_position + Vector2(0, -16)
+	var map_pos = tile_map.local_to_map(tile_map.to_local(detection_pos))
+	var source_id = tile_map.get_cell_source_id(map_pos)
+	var tile_data = tile_map.get_cell_tile_data(map_pos)
 	
-	move_input = Input.get_axis("move_left", "move_right")
-	
-	if move_input != 0:
-		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+	if source_id != -1 and tile_data and tile_data.get_custom_data("Climbable"):
+		is_on_rope = true
 	else:
-		velocity.x = lerp(velocity.x, 0.0, braking * delta)
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
+		is_on_rope = false
+
+	move_input = Input.get_axis("move_left", "move_right")
+
+	if is_on_rope:
+		velocity.y = 0
+		var climb_dir = Input.get_axis("jump", "move_down")
+		if climb_dir != 0:
+			velocity.y = climb_dir * move_speed
+		
+		velocity.x = move_input * (move_speed * 0.5)
+	else:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		
+		if move_input != 0:
+			velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+		else:
+			velocity.x = lerp(velocity.x, 0.0, braking * delta)
+			
+		if Input.is_action_pressed("jump") and is_on_floor():
+			velocity.y = -jump_force
 	
 	move_and_slide()
 
@@ -45,7 +65,9 @@ func _process(delta):
 	_manage_animation()
 
 func _manage_animation():
-	if not is_on_floor():
+	if is_on_rope:
+		anim.play("idle")
+	elif not is_on_floor():
 		anim.play("jump")
 	elif move_input != 0:
 		anim.play("move")
@@ -56,7 +78,6 @@ func take_damage(amount : int):
 	health -= amount
 	OnUpdateHealth.emit(health)
 	_damage_flash()
-
 	if health <= 0:
 		call_deferred("game_over")
 	play_sound(take_damage_sfx)
